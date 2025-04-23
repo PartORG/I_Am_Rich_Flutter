96759caaeb5f7c4c246e030ac86349ff3d0f046f
Ievgen Perederieiev <peredereev.ievgen@gmail.com>
2025-04-23 11:44:00 +0200

    added main code

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+// The main function is the starting point for all our Flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[400],
+        appBar: AppBar(
+          title: Text(
+            'I Am Rich',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
